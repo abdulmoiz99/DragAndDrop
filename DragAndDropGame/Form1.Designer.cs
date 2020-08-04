ffd77c5c823bbe3ced1d53b887f854098f48a81c
Naqqash Khan <naqqash10@hotmail.com>
2020-08-04 16:45:07 +0500

    Apna khana khud bnao

--- DragAndDropGame/Form1.Designer.cs
+++ DragAndDropGame/Form1.Designer.cs
@@ -28,13 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Control";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(396, 277);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(106, 108);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Visible = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(484, 171);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(105, 82);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.Visible = false;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(631, 193);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(111, 95);
+            this.listBox3.TabIndex = 3;
+            this.listBox3.Visible = false;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(544, 405);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(179, 147);
+            this.listBox4.TabIndex = 4;
+            this.listBox4.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(440, 327);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(528, 218);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(665, 244);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(615, 484);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1190, 733);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- DragAndDropGame/Form1.cs
+++ DragAndDropGame/Form1.cs
@@ -12,9 +12,95 @@ namespace DragAndDropGame
 {
     public partial class Form1 : Form
     {
+        private Control activeControl;
+        private Point previousPosition;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var label = new Label();
+            label.AutoSize = true;
+            label.Location = new Point(118, 182);
+            label.Text = "Sample";
+            label.MouseDown += new MouseEventHandler(Label_MouseDown);
+            label.MouseUp += new MouseEventHandler(Label_MouseUp);
+            label.MouseMove += new MouseEventHandler(Label_MouseMove);
+            Controls.Add(label);
+        }
+
+        private void Label_MouseMove(object sender, MouseEventArgs e)
+        {          
+            if (activeControl == null || activeControl != sender)
+            {
+                return;
+            }
+
+            var location = activeControl.Location;
+            location.Offset(e.Location.X - previousPosition.X, e.Location.Y - previousPosition.Y);
+            activeControl.Location = location;
+        }
+
+        private void Label_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (activeControl.Bounds.IntersectsWith(listBox1.Bounds))
+            {
+                activeControl.Location = radioButton1.Location;
+            }
+            else if (activeControl.Bounds.IntersectsWith(listBox2.Bounds))
+            {
+                activeControl.Location = radioButton2.Location;
+            }
+            else if (activeControl.Bounds.IntersectsWith(listBox3.Bounds))
+            {
+                activeControl.Location = radioButton3.Location;
+            }
+            else if (activeControl.Bounds.IntersectsWith(listBox4.Bounds))
+            {
+                activeControl.Location = radioButton4.Location;
+            }
+            else 
+            {
+                activeControl.Location = new Point(118, 182);
+            }
+            activeControl = null;
+            Cursor = Cursors.Default;
+        }
+
+        private void Label_MouseDown(object sender, MouseEventArgs e)
+        {
+            activeControl = sender as Control;
+            previousPosition = e.Location;
+            Cursor = Cursors.Hand;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
